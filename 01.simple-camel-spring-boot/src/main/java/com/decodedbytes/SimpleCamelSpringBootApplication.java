222d588d01bf3b6a68d9b82f1202afd653f11aaf
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-01-17 12:14:04 +0530

    Add files via upload

---
+++ 01.simple-camel-spring-boot/src/main/java/com/decodedbytes/SimpleCamelSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.decodedbytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCamelSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCamelSpringBootApplication.class, args);
+	}
+
+}
